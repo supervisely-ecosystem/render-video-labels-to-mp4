a51318bffea0abfd6ba207215d5fdcc4cfffd132
GoldenAnpu <seryios.sych@gmail.com>
2025-01-10 15:52:30 +0100

    Update supervisely version and add VSCode configuration files Update class names style

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 import os
 from distutils import util
+
 import cv2
 import numpy as np
-
 import supervisely as sly
+from dotenv import load_dotenv
+from PIL import Image, ImageDraw, ImageFont
 from supervisely.app.v1.app_service import AppService
-from supervisely.video_annotation.key_id_map import KeyIdMap
 from supervisely.geometry.constants import BITMAP
 from supervisely.imaging.color import generate_rgb
+from supervisely.video_annotation.key_id_map import KeyIdMap
+
+if sly.is_development():
+    load_dotenv("debug.env")
+    load_dotenv("/home/serpntns/supervisely.env")
 
 TEAM_ID = sly.env.team_id()
 WORKSPACE_ID = sly.env.workspace_id()
@@ -25,6 +31,9 @@ PROJECT_ID = None
 CLASSES = []
 COLOR_INS = True
 FONT = cv2.FONT_HERSHEY_COMPLEX
+FONT_PATH = "fonts/FiraSans-Bold.ttf"
+absolute_font_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), FONT_PATH)
+font_size = 32
 
 
 @my_app.callback("render_video_labels_to_mp4")
@@ -33,7 +42,9 @@ def render_video_labels_to_mp4(api: sly.Api, task_id, context, state, app_logger
     global VIDEO_ID, START_FRAME, END_FRAME, PROJECT_ID
     original_video_id = VIDEO_ID
     if VIDEO_ID == "":
-        raise ValueError("Please, copy Video ID from your project and paste it to the modal window.")
+        raise ValueError(
+            "Please, copy Video ID from your project and paste it to the modal window."
+        )
     VIDEO_ID = "".join(filter(str.isnumeric, VIDEO_ID))
     if not VIDEO_ID.isnumeric():
         raise ValueError(
@@ -43,7 +54,9 @@ def render_video_labels_to_mp4(api: sly.Api, task_id, context, state, app_logger
     VIDEO_ID = int(VIDEO_ID)
     video_info = api.video.get_info_by_id(VIDEO_ID)
     if video_info is None:
-        raise ValueError(f"Video with id={original_video_id} not found. Please, copy Video ID from your project and paste it to the modal window.")
+        raise ValueError(
+            f"Video with id={original_video_id} not found. Please, copy Video ID from your project and paste it to the modal window."
+        )
     PROJECT_ID = video_info.project_id
     project_info = api.project.get_info_by_id(PROJECT_ID)
     if project_info.workspace_id != WORKSPACE_ID:
@@ -58,8 +71,11 @@ def render_video_labels_to_mp4(api: sly.Api, task_id, context, state, app_logger
         if START_FRAME == 0 and END_FRAME == 0:
             raise ValueError("Frame Range is not defined")
         if END_FRAME >= video_info.frames_count:
-            app_logger.warn("End Frame {} is out of range: video has only {} frames"
-                            .format(END_FRAME, video_info.frames_count))
+            app_logger.warn(
+                "End Frame {} is out of range: video has only {} frames".format(
+                    END_FRAME, video_info.frames_count
+                )
+            )
             END_FRAME = video_info.frames_count - 1
             app_logger.warn("End Frame has been set to {}".format(END_FRAME))
 
@@ -78,6 +94,7 @@ def render_video_labels_to_mp4(api: sly.Api, task_id, context, state, app_logger
     obj_to_color = {}
     exist_colors = []
     video = None
+    font = ImageFont.truetype(absolute_font_path, font_size)
 
     mp4_name = sly.fs.get_file_name(video_info.name) + ".mp4"
     local_path = os.path.join(my_app.data_dir, mp4_name)
@@ -99,80 +116,136 @@ def render_video_labels_to_mp4(api: sly.Api, task_id, context, state, app_logger
                             color = obj_to_color[fig.video_object.key]
 
                     bbox = None
-                    if fig.geometry.geometry_name() == BITMAP or fig.geometry.geometry_name() == 'polygon':
+                    if (
+                        fig.geometry.geometry_name() == BITMAP
+                        or fig.geometry.geometry_name() == "polygon"
+                    ):
                         mask = np.zeros(frame_np.shape, dtype=np.uint8)
                         fig.geometry.draw(mask, color)
                         frame_np = cv2.addWeighted(frame_np, 1, mask, OPACITY, 0)
                         if SHOW_NAMES == True:
-                           bbox = fig.geometry.to_bbox()
-                           bbox.draw_contour(frame_np, color, THICKNESS)
+                            bbox = fig.geometry.to_bbox()
+                            bbox.draw_contour(frame_np, color, THICKNESS)
 
-                    elif fig.geometry.geometry_name() == 'rectangle':
+                    elif fig.geometry.geometry_name() == "rectangle":
                         bbox = fig.geometry
                         bbox.draw_contour(frame_np, color, THICKNESS)
 
                     else:
-                        raise TypeError("Geometry type {} not supported".format(fig.geometry.geometry_name()))
-                    
+                        raise TypeError(
+                            "Geometry type {} not supported".format(fig.geometry.geometry_name())
+                        )
+
                     if SHOW_NAMES == True:
-                        tl = 1  # line/font thickness
-                        c1, c2 = (bbox.left, bbox.top), (bbox.right, bbox.bottom)
-                        tf = 1  # font thickness
-                        t_size = cv2.getTextSize(fig.video_object.obj_class.name, FONT, fontScale=tl, thickness=tf)[0]
-                        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+                        # tl = 1  # line/font thickness
+                        # c1, c2 = (bbox.left, bbox.top), (bbox.right, bbox.bottom)
+                        # tf = 1  # font thickness
+                        # t_size = cv2.getTextSize(
+                        #     fig.video_object.obj_class.name, FONT, fontScale=tl, thickness=tf
+                        # )[0]
+                        # c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
 
-                        cv2.rectangle(frame_np, c1, c2, fig.video_object.obj_class.color, -1, cv2.LINE_AA)  # filled
+                        # cv2.rectangle(
+                        #     frame_np, c1, c2, fig.video_object.obj_class.color, -1, cv2.LINE_AA
+                        # )  # filled
 
-                        cv2.putText(frame_np, fig.video_object.obj_class.name, (bbox.left + 1, bbox.top - 1),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 1,
-                                    [255, 255, 255],
-                                    thickness=THICKNESS, lineType=cv2.LINE_AA, bottomLeftOrigin=False)
+                        # cv2.putText(
+                        #     frame_np,
+                        #     fig.video_object.obj_class.name,
+                        #     (bbox.left + 1, bbox.top - 1),
+                        #     cv2.FONT_HERSHEY_SIMPLEX,
+                        #     1,
+                        #     [255, 255, 255],
+                        #     thickness=THICKNESS,
+                        #     lineType=cv2.LINE_AA,
+                        #     bottomLeftOrigin=False,
+                        # )
+
+                        image_pil = Image.fromarray(cv2.cvtColor(frame_np, cv2.COLOR_BGR2RGB))
+                        draw = ImageDraw.Draw(image_pil)
+                        text = fig.video_object.obj_class.name
+                        text_color = (79, 79, 79)
+                        t_bbox = draw.textbbox((0, 0), text, font=font)
+                        t_width = t_bbox[2] - t_bbox[0]
+                        t_height = t_bbox[3] - t_bbox[1]
+                        padding_x = 10
+                        padding_y = 5
+                        c1 = (bbox.left, bbox.top - t_height - 2 * padding_y)
+                        c2 = (c1[0] + t_width + 2 * padding_x, c1[1] + t_height + 2 * padding_y)
+                        position = (bbox.left + padding_x, bbox.top - t_height - padding_y)
+                        rect_color = (color[2], color[1], color[0])
+                        draw.rectangle([c1, c2], fill=rect_color)
+                        draw.text(position, text, font=font, fill=text_color)
+                        frame_np = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
 
         if video is None:
-            video = cv2.VideoWriter(local_path,
-                                    cv2.VideoWriter_fourcc(*'MP4V'),
-                                    stream_speed,
-                                    (frame_np.shape[1], frame_np.shape[0]))
+            video = cv2.VideoWriter(
+                local_path,
+                cv2.VideoWriter_fourcc(*"MP4V"),
+                stream_speed,
+                (frame_np.shape[1], frame_np.shape[0]),
+            )
 
         frame_np = cv2.cvtColor(frame_np, cv2.COLOR_BGR2RGB)
         video.write(frame_np)
         progress.iter_done_report()
 
     if video is None:
-        raise ValueError('No frames to create video')
+        raise ValueError("No frames to create video")
     video.release()
 
-    remote_path = os.path.join(sly.team_files.RECOMMENDED_EXPORT_PATH, 'rendered_videos', "{}_{}".format(VIDEO_ID, mp4_name))
+    remote_path = os.path.join(
+        sly.team_files.RECOMMENDED_EXPORT_PATH,
+        "rendered_videos",
+        "{}_{}".format(VIDEO_ID, mp4_name),
+    )
     remote_path = api.file.get_free_name(TEAM_ID, remote_path)
     upload_progress = []
+
     def _print_progress(monitor, upload_progress):
         if len(upload_progress) == 0:
-            upload_progress.append(sly.Progress(message="Upload {!r}".format(mp4_name),
-                                                total_cnt=monitor.len,
-                                                ext_logger=app_logger,
-                                                is_size=True))
+            upload_progress.append(
+                sly.Progress(
+                    message="Upload {!r}".format(mp4_name),
+                    total_cnt=monitor.len,
+                    ext_logger=app_logger,
+                    is_size=True,
+                )
+            )
         upload_progress[0].set_current_value(monitor.bytes_read)
 
-    file_info = api.file.upload(TEAM_ID, local_path, remote_path, lambda m: _print_progress(m, upload_progress))
+    file_info = api.file.upload(
+        TEAM_ID, local_path, remote_path, lambda m: _print_progress(m, upload_progress)
+    )
     app_logger.info("Uploaded to Team-Files: {!r}".format(remote_path))
-    api.task._set_custom_output(task_id, file_info.id, file_info.name, file_url=file_info.storage_path,
-                                description=f"File mp4: {remote_path}", icon="zmdi zmdi-cloud-download", download=True)
+    api.task._set_custom_output(
+        task_id,
+        file_info.id,
+        file_info.name,
+        file_url=file_info.storage_path,
+        description=f"File mp4: {remote_path}",
+        icon="zmdi zmdi-cloud-download",
+        download=True,
+    )
     sly.fs.silent_remove(local_path)
     my_app.stop()
 
 
 def main():
-    sly.logger.info("Script arguments", extra={
-        "TEAM_ID": TEAM_ID,
-        "WORKSPACE_ID": WORKSPACE_ID,
-        "VIDEO_ID": VIDEO_ID,
-        "ALL_FRAMES": ALL_FRAMES,
-        "START_FRAME": START_FRAME,
-        "END_FRAME": END_FRAME,
-        "SHOW_NAMES": SHOW_NAMES,
-        "THICKNESS": THICKNESS,
-        "OPACITY": OPACITY
-    })
+    sly.logger.info(
+        "Script arguments",
+        extra={
+            "TEAM_ID": TEAM_ID,
+            "WORKSPACE_ID": WORKSPACE_ID,
+            "VIDEO_ID": VIDEO_ID,
+            "ALL_FRAMES": ALL_FRAMES,
+            "START_FRAME": START_FRAME,
+            "END_FRAME": END_FRAME,
+            "SHOW_NAMES": SHOW_NAMES,
+            "THICKNESS": THICKNESS,
+            "OPACITY": OPACITY,
+        },
+    )
     my_app.run(initial_events=[{"command": "render_video_labels_to_mp4"}])
 
 
